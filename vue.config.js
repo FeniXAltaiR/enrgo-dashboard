875df19ffb229c3397dd0ddb69a7b229649fdc88
Oleg Sazonov <gameoveran@yandex.ru>
2023-08-04 15:31:12 +0300

    Add .prettierrc.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: (config) => {
-    config.plugin("html").tap((args) => {
-      args[0].filename = "monitoring.html";
-      return args;
-    });
+    config.plugin('html').tap((args) => {
+      args[0].filename = 'monitoring.html'
+      return args
+    })
   },
-  assetsDir: "monitoring",
-});
+  assetsDir: 'monitoring',
+})
